c07204bc2f46722f07c48735b22201c1bea4d800
mehedi <mehedi25122003@gmail.com>
2025-05-04 21:58:30 +0600

    start

--- coffeStore/src/Components/AddCoffe.jsx
+++ coffeStore/src/Components/AddCoffe.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddCoffe = () => {
     return (
         <div>
-            
+            <h1>add coffe section</h1>
         </div>
     );
 };
--- coffeStore/src/Components/UpdateCoffe.jsx
+++ coffeStore/src/Components/UpdateCoffe.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UpdateCoffe = () => {
     return (
         <div>
-            
+            <h2>update coffe section</h2>
         </div>
     );
 };
