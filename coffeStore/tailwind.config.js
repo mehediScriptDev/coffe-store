a91214417199111ea315eef685a03423a52af42f
mehedi <mehedi25122003@gmail.com>
2025-05-04 22:27:49 +0600

    formbg added

--- coffeStore/tailwind.config.js
+++ coffeStore/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        formbg: 'rgb(244, 243, 240)',
+      }
+    },
   },
   plugins: [
     require('daisyui'),
