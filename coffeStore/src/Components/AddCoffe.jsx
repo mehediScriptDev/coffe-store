f82f8a57a8dceea48c41b519e9811c10d8225203
mehedi <mehedi25122003@gmail.com>
2025-05-04 22:30:35 +0600

    form shape almost done

--- coffeStore/src/Components/AddCoffe.jsx
+++ coffeStore/src/Components/AddCoffe.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const AddCoffe = () => {
     return (
-        <section className='w-11/12 mx-auto'>
-            <p>Add coffe</p>
+        <section className='w-11/12 mx-auto bg-formbg py-3 px-6'>
+            <p className='text-3xl text-center font-bold'>Add coffe</p>
             <form>
                 <label htmlFor="">
                     <p>Coffe Name:</p>
                     <input type="text" placeholder='enter coffe name' className='border-2 px-10 rounded-md w-full h-7' />
                 </label>
                 <label htmlFor="">
-                    <p>Coffe quantity</p>
+                    <p>Coffe quantity:</p>
                     <input type="text" placeholder='enter coffe quantity' className='border-2 px-10 rounded-md w-full h-7' />
                 </label>
             </form>
