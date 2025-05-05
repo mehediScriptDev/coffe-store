503ecb79d7e34afb821e29d0a1bb7e2e8666b332
mehedi <mehedi25122003@gmail.com>
2025-05-05 10:31:36 +0600

    error page added

--- coffeStore/src/Components/Error.jsx
+++ coffeStore/src/Components/Error.jsx
@@ -1,11 +1,13 @@
-
+import banner from "./images/404/404.gif";
 
 const Error = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <section className="w-11/12 mx-auto">
+      <div className="flex justify-center w-full h-screen  items-center">
+        <img className="flex justify-center" src={banner} alt="404 error pic" />
+      </div>
+    </section>
+  );
 };
 
 export default Error;
